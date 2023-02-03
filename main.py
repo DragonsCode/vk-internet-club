8682747e0ea9fc601d4d9db769b1a35f1b7d0c17
DragonsCode <84837099+DragonsCode@users.noreply.github.com>
2023-02-03 15:58:32 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -31,31 +31,6 @@ async def private_message_handler(message: Message):
     await message.answer("🔥Свой собственный клуб интернета получили уже десятки пользователей, радуются и пишут отзывы\n\n👀Несколько я прикрепил, а остальные - читайте тут:\nhttps://vk.com/topic-211717723_48984252")
 
 
-# @bot.on.private_message(text='/nax')
-# async def no_board(message: Message):
-#     await message.answer('Netu nax', keyboard=EMPTY_KEYBOARD)
-
-@bot.on.private_message(text="Реферальная система")
-async def ref(message: Message):
-    user_id = message.peer_id
-    await message.answer(f"Приглашайте людей по вашей ссылке и получайте скидки!\nВаша ссылка: vk.me/intervpn?ref={user_id}")
-
-
-@bot.on.private_message()
-async def smth(message: Message):
-    ref = message.ref
-    if ref and ref.isdigit():
-        if int(ref) == message.peer_id:
-            await message.answer('Cheater!')
-            return
-        is_group = await bot.api.groups.is_member(188552039, int(ref))
-        if is_group:
-            await message.answer(f"Вы пришли по ссылке человека: {ref}")
-        else:
-            await message.answer(f'User with id {ref} does not exist in this bot')
-    else:
-        await message.answer("Привет")
-
 @bot.on.private_message()
 async def check_db(message: Message):
     insert_user(message.peer_id)
