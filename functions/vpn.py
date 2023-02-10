c929b9ab80ddebcc545156c0ab4fcadbf2dad198
DragonsCode <84837099+DragonsCode@users.noreply.github.com>
2023-02-10 19:53:23 +0500

    Update vpn.py

--- functions/vpn.py
+++ functions/vpn.py
@@ -1,9 +1,9 @@
 from functions.outline_vpn.outline_vpn import OutlineVPN
 
 
-def new_key(url):
+def new_key(url, name=None):
     client = OutlineVPN(api_url=url)
-    key = client.create_key()
+    key = client.create_key(name)
     return key.key_id, key.access_url
 
 def del_key(url, key_id):
