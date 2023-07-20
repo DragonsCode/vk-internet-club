1c86f2d64720bb5e7ae6b2288a21f442c8d74b5c
Drago <84837099+DragonsCode@users.noreply.github.com>
2023-07-20 20:21:01 +0500

    Update my_club.py

--- handlers/my_club.py
+++ handlers/my_club.py
@@ -56,7 +56,7 @@ async def my_club_handler(message: Message):
         user = get_user(message.peer_id)
     sub = user.end_date
     s = None
-    dons = await api.groups.get_members("-211717723", filter="donut")
+    dons = await api.groups.get_members("211717723", filter="donut")
     dons_ids = [i.id for i in dons.items]
     if sub is not None:
         s = user.end_date > datetime.now()
